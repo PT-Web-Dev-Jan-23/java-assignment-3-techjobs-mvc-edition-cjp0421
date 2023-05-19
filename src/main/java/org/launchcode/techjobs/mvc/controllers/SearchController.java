4ad83cb5f7cc26d07bc681da265096cf81935b79
cjp0421 <76797183+cjp0421@users.noreply.github.com>
2023-05-19 15:22:45 -0500

    redo task 4 - website seems to work, passes 18/20 tests - not sure why the other two fail

--- src/main/java/org/launchcode/techjobs/mvc/controllers/SearchController.java
+++ src/main/java/org/launchcode/techjobs/mvc/controllers/SearchController.java
@@ -29,8 +29,19 @@ public class SearchController {
 
     // TODO #3 - Create a handler to process a search request and render the updated search view.
     @PostMapping("/results")
-    public String displaySearchResults(Model model, @RequestParam String searchTerm, @RequestParam String searchType){
+    public String displaySearchResults(Model model, @RequestParam String searchType, @RequestParam(required = false) String searchTerm){
+        ArrayList<Job> jobs;
+        if (searchType.equals("all")){
+            jobs = JobData.findByValue(searchTerm);
+            model.addAttribute("title", "All Jobs");
+        } else {
+            jobs = JobData.findByColumnAndValue(searchType, searchTerm);
+            model.addAttribute("title", "Jobs with " + columnChoices.get(searchType) + ": " + searchTerm);
+        }
+
+
         model.addAttribute("columns", columnChoices);
+        model.addAttribute("jobs",jobs);
         return "search";
     }
 }
